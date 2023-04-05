9ed97bbc0e8eb8ea982687d234f1511d1371e19f
MCampbell <cmvzjx@gmail.com>
2023-04-04 20:44:28 -0500

    Mc ids (#52)
    * IDS Controller
    
    Co-authored-by: SGBM-Jimbo <76476900+SGBM-Jimbo@users.noreply.github.com>

--- controllers/IdsController.js
+++ controllers/IdsController.js
@@ -4,6 +4,7 @@ import User from '../models/User.js';
 import Pireps from '../models/Pireps.js';
 import Config from '../models/Config.js';
 import config from '../config/zab.js';
+import axios from 'axios'
 
 const router = express.Router();
 
@@ -123,7 +124,6 @@ router.post('/vatis', async (req, res) => {
 	  console.log(`Invalid airport: ${Facility}`);
 	  return res.status(400).send({ error: `Invalid airport: ${Facility}` });
 	}
-  
 	console.log("Facility is present and acceptable");
   
 	let redisAtis = await req.app.redis.get('AtisLetter');
@@ -171,6 +171,37 @@ router.get('/pireps', async({res}) => {
 	const pirep = await Pireps.find().sort('-reportTime').lean();
 	return res.json(pirep);
 });
+
+router.get('/vatsim-data', (req, res) => {
+	axios.get('https://status.vatsim.net/status.json')
+	  .then(response => {
+		const apiUrl = response.data.data.v3[0];
+		axios.get(apiUrl)
+		  .then(vatsimResponse => {
+			res.json(vatsimResponse.data);
+		  })
+		  .catch(error => {
+			console.error(error);
+			res.status(500).send('Error fetching data from external API.');
+		  });
+	  })
+	  .catch(error => {
+		console.error(error);
+		res.status(500).send('Error fetching data from external API.');
+	  });
+});
+
+router.get('/charts/:airportCode', async (req, res) => {
+	const airportCode = req.params.airportCode.toUpperCase();
+	try {
+	  const response = await axios.get(`https://api.aviationapi.com/v1/charts?apt=${airportCode}`);
+	  const charts = response.data[airportCode];
+	  res.json(charts);
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).send('Error fetching data from external API.');
+	}
+  });
 
 router.post('/pireps', async(req, res) => {
 	if(req.body.ua === undefined || req.body.ov === undefined) {
