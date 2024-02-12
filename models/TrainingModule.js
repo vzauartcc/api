d233966136328457efa0bb019864bd7f98b1128d
Tim Barker <tbarker1989@gmail.com>
2024-02-12 01:24:10 -0500

    2/12

--- models/TrainingModule.js
+++ models/TrainingModule.js
@@ -1,19 +1,24 @@
 import m from 'mongoose';
 
+// Define a Course schema that includes _id
+const courseSchema = new m.Schema({
+  _id: { type: m.Schema.Types.ObjectId, auto: true },
+  courseName: { type: String },
+  description: { type: String },
+  type: { type: String },
+  facility: { type: String }
+});
+
+// Use courseSchema for the courses array in your TrainingModule schema
 const trainingModuleSchema = new m.Schema({
-    name: String,
-    description: String,
-    track: String, // "home" or "visiting"
-    courses: [{
-      courseName: { type: String },
-      description: { type: String },
-      type: { type: String },
-      facility: { type: String }
-    }],
-    isExtension: { type: Boolean, default: false },
-    prerequisites: [{ type: m.Schema.Types.ObjectId, ref: 'TrainingModule' }], // References to prerequisite modules
-    extensionModule: { type: m.Schema.Types.ObjectId, ref: 'TrainingModule', default: null }, // Link to a module if this is an extension
-},{
+  name: String,
+  description: String,
+  track: String, // "home" or "visiting"
+  courses: [courseSchema], // Now each course will have an _id
+  isExtension: { type: Boolean, default: false },
+  prerequisites: [{ type: m.Schema.Types.ObjectId, ref: 'TrainingModule' }],
+  extensionModule: { type: m.Schema.Types.ObjectId, ref: 'TrainingModule', default: null },
+}, {
   collection: 'trainingModule'
 });
   
