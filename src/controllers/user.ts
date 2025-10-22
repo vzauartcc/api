fd57b7b13fd40b315382c0f4465cf0c1adffc206
Ryan Savara <ryan.savara@vatusa.net>
2025-10-22 18:25:07 -0400

    Abstract out activity requirement amount and names, hopefully making future changes easier.

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,12 +2,14 @@ import axios from 'axios';
 import { randomUUID } from 'crypto';
 import { Router, type Request, type Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { convertToReturnDetails, getQuarterStart, uploadToS3 } from '../app.js';
+import { convertToReturnDetails, uploadToS3 } from '../app.js';
 import getUser, { deleteAuthCookie, type UserPayload } from '../middleware/user.js';
 import oAuth from '../middleware/vatsim.js';
 import { ControllerHoursModel } from '../models/controllerHours.js';
 import { NotificationModel } from '../models/notification.js';
+import { TrainingSessionModel } from '../models/trainingSession.js';
 import { UserModel } from '../models/user.js';
+import zau from '../zau.js';
 
 const router = Router();
 
@@ -200,15 +202,26 @@ router.get('/logout', async (req: Request, res: Response) => {
 
 router.get('/sessions', getUser, async (req: Request, res: Response) => {
 	try {
-		const quarterStart = getQuarterStart();
 		const sessions = await ControllerHoursModel.find({
 			cid: req.user!.cid,
-			timeStart: { $gt: quarterStart },
+			timeStart: { $gt: zau.activity.period.startOfCurrent },
 		})
 			.sort({ timeStart: -1 })
 			.lean()
 			.exec();
-		res.stdRes.data = sessions;
+		const training = await TrainingSessionModel.find({
+			studentCid: req.user!.cid,
+			startTime: zau.activity.period.startOfCurrent,
+		})
+			.sort({ startTime: -1 })
+			.lean()
+			.exec();
+		res.stdRes.data = {
+			sessions,
+			training,
+			period: zau.activity.period,
+			requirements: zau.activity.requirements,
+		};
 	} catch (e) {
 		res.stdRes.ret_det = convertToReturnDetails(e);
 		req.app.Sentry.captureException(e);
